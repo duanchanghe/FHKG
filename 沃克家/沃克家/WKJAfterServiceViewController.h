df2c641ca6a28468b9545ad9ed3b99cc80f15568
duanchanghe <jundanuantong@appledeimac-2.local>
2016-10-07 10:22:08 +0800

    创建基本类，添加图片

---
+++ 沃克家/沃克家/WKJAfterServiceViewController.h
@@ -0,0 +1,13 @@
+//
+//  WKJAfterServiceViewController.h
+//  沃克家
+//
+//  Created by jundanuantong on 16/10/7.
+//  Copyright © 2016年 jundanuantong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WKJAfterServiceViewController : UIViewController
+
+@end
---
+++ 沃克家/沃克家/WKJSalesViewController.h
@@ -0,0 +1,13 @@
+//
+//  WKJSalesViewController.h
+//  沃克家
+//
+//  Created by jundanuantong on 16/10/7.
+//  Copyright © 2016年 jundanuantong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WKJSalesViewController : UIViewController
+
+@end
